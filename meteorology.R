8887de4e5cf1fe3d3e9c48577534de2ac4b7877e
Gustavo Olivares <guolivar@gmail.com>
2015-09-17 13:20:11 +1200

    Met plays ... adding rangiora_0

--- meteorology.R
+++ meteorology.R
@@ -1,9 +1,13 @@
 #CONA MET
 library('openair')
-
-rangiora.1_1 <- read.csv("~/data/CONA/from_work/MET/rangiora 1_1.csv")
-rangiora.2_1 <- read.csv("~/data/CONA/from_work/MET/rangiora 2_1.csv")
-rangiora.3_1 <- read.csv("~/data/CONA/from_work/MET/rangiora 3_1.csv")
+rangiora.0.1 <- read.csv("~/data/CONA/from_work/MET/rangiora_0.csv")
+rangiora.1_1 <- read.csv("~/data/CONA/from_work/MET/rangiora_1.csv")
+rangiora.2_1 <- read.csv("~/data/CONA/from_work/MET/rangiora_2.csv")
+rangiora.3_1 <- read.csv("~/data/CONA/from_work/MET/rangiora_3.csv")
+rangiora.0_1$date <- as.POSIXct(paste(rangiora.1_1$Date,rangiora.1_1$Time),format = '%d/%m/%Y %H:%M:%S')
+rangiora.0_1$Date<-NULL
+rangiora.0_1$Time<-NULL
+names(rangiora.0_1)<-c('wd.0.1','Tmin.0.1','Tmax.0.1','Tavg.0.1','rh.0.1','pres.0.1','sd.wd.0.1','gust.wd.0.1','ws.0.1','sd.ws.0.1','gust.ws.0.1','batt.0.1','date')
 rangiora.1_1$date <- as.POSIXct(paste(rangiora.1_1$Date,rangiora.1_1$Time),format = '%d/%m/%Y %H:%M:%S')
 rangiora.1_1$Date<-NULL
 rangiora.1_1$Time<-NULL
@@ -15,6 +19,7 @@ names(rangiora.2_1)<-c('wd.2.1','Tmin.2.1','Tmax.2.1','Tavg.2.1','rh.2.1','pres.
 rangiora.3_1$date <- as.POSIXct(paste(rangiora.3_1$Date,rangiora.3_1$Time),format = '%d/%m/%Y %H:%M:%S')
 rangiora.3_1$Date<-NULL
 rangiora.3_1$Time<-NULL
+timePlot(met_data,pollutant = c('ws.1.1','ws.2.1','ws.3.1'),group = TRUE)
 names(rangiora.3_1)<-c('wd.3.1','Tmin.3.1','Tmax.3.1','Tavg.3.1','rh.3.1','pres.3.1','sd.wd.3.1','gust.wd.3.1','ws.3.1','sd.ws.3.1','gust.ws.3.1','batt.3.1','date')
 met_data <- merge(rangiora.1_1,rangiora.2_1,by='date',all = TRUE)
 met_data <- merge(met_data,rangiora.3_1,by='date',all = TRUE)
@@ -23,7 +28,6 @@ names(met_data)
 #Plots
 timePlot(met_data,pollutant = c('Tavg.1.1','Tavg.2.1','Tavg.3.1'),group = TRUE)
 timePlot(met_data,pollutant = c('wd.1.1','wd.2.1','wd.3.1'),group = TRUE)
-timePlot(met_data,pollutant = c('ws.1.1','ws.2.1','ws.3.1'),group = TRUE)
 timePlot(met_data,pollutant = c('rh.1.1','rh.2.1','rh.3.1'),group = TRUE)
 timePlot(met_data,pollutant = c('wd.1.1','wd.2.1','wd.3.1'),group = TRUE,avg.time = '10 min')
 timePlot(met_data,pollutant = c('ws.1.1','ws.2.1','ws.3.1'),group = TRUE,avg.time = '10 min')
