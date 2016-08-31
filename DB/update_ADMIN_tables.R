fd03c8475b93f7d11d5463f2353a80f082cb286f
gustavo <gustavo@ip-10-242-128-162.ap-southeast-2.compute.internal>
2016-08-31 01:16:08 +0000

    Basic admin data uploaded and script debugged

--- DB/update_ADMIN_tables.R
+++ DB/update_ADMIN_tables.R
@@ -1,58 +1,85 @@
 ##### Load relevant packages #####
 library("RPostgreSQL")
+##### Set the working directory DB ####
+setwd("~/repositories/cona/DB")
+##### Read the credentials file (hidden) ####
+access <- read.delim("./.cona_login")
 ##### Open the connection to the DB ####
 p <- dbDriver("PostgreSQL")
-con<-dbConnect(p,user='adm_penap',password='p3n@p@dm',host='localhost',dbname='didactic',port=5432)
-#con<-dbConnect(p,user='penap',password='p3n@p',host='localhost',dbname='didactic',port=5432)
-##### Update Institutions #####
+con<-dbConnect(p,
+               user=as.character(access$user[1]),
+               password=as.character(access$pwd[1]),
+               host='localhost',
+               dbname='cona',
+               port=5432)
+##### ADD Institutions #####
 # Read info from file
-institutions <- read.delim("./institutions.txt")
-nrecs<-length(institutions[,1])
-# Run SQL queries on the DB
-for (i in 1:nrecs){
-  rs<-dbGetQuery(con,paste0("insert into admin.institutions
-               values(DEFAULT,'",institutions[i,1],"','",institutions[i,2],"');"))
+# Check if empty file
+if (file.size("./institutions.txt")>1){
+  institutions <- read.delim("./institutions.txt")
+  nrecs<-length(institutions[,1])
+  # Run SQL queries on the DB
+  for (i in 1:nrecs){
+    rs<-dbGetQuery(con,paste0("insert into admin.institution
+                              values(DEFAULT,'",institutions[i,1],"','",institutions[i,2],"');"))
+  }
 }
 
-##### Update PEOPLE #####
+##### ADD Woodburner #####
 # Read info from file
-people <- read.delim("./people.txt")
-nrecs<-length(people[,1])
-# Run inserts to the DB
-for (i in 1:nrecs){
-  institutionid<-dbGetQuery(con,paste0("select id from admin.institutions as ins
-                                       where ins.name='",people$InstitutionName[i],"' and
-                                       ins.notes='",people$InstitutionNotes[i],"';"))
-  rs<-dbGetQuery(con,paste0("insert into admin.people
-               values(DEFAULT,'",people$Name[i],"',",institutionid,",'",people$Notes,"');"))
+if (file.size("./woodburners.txt")>1){
+  woodburners <- read.delim("./woodburners.txt")
+  nrecs<-length(woodburners[,1])
+  # Run inserts to the DB
+  for (i in 1:nrecs){
+    rs<-dbGetQuery(con,paste0("insert into woodburner
+                              values(DEFAULT,'",woodburners$makemodel[i],"');"))
+  }
 }
-##### Update Instruments #####
+##### ADD Instruments #####
 # Read info from file
-instruments <- read.delim("./instruments.txt")
-nrecs<-length(instruments[,1])
-# Run SQL queries on the DB
-for (i in 1:nrecs){
-  institutionid<-dbGetQuery(con,paste0("select id from admin.institutions as ins
-                                       where ins.name='",instruments$OwnerName[i],"' and
-                                       ins.notes='",instruments$OwnerNotes[i],"';"))
-  rs<-dbGetQuery(con,paste0("insert into admin.instruments
-               values(DEFAULT,'",instruments$SN[i],"','",instruments$Name[i],"','",instruments$Notes[i],"',",institutionid,");"))
+if (file.size("./instruments.txt")>1){
+  instruments <- read.delim("./instruments.txt")
+  nrecs<-length(instruments[,1])
+  # Run SQL queries on the DB
+  for (i in 1:nrecs){
+    institutionid<-dbGetQuery(con,paste0("select id from admin.institution as ins
+                                         where ins.name='",instruments$OwnerName[i],"';"))
+    rs<-dbGetQuery(con,paste0("insert into admin.instrument
+                 values(DEFAULT,'",instruments$SN[i],"','",instruments$Name[i],"','",instruments$Notes[i],"',",institutionid,");"))
+  }
 }
 
-##### Update Sensors #####
+##### ADD Sensors #####
 # Read info from file
 # Run SQL queries on the DB
 # Get instrument IDs
-inst_idName<-dbGetQuery(con,"select id, name from admin.instruments")
+inst_idName<-dbGetQuery(con,"select id, name from admin.instrument")
 ninst<-length(inst_idName$id)
 for (i in 1:ninst){
 # i=inst_idName$id[1]
   # For each instrument, load the sensors info from the file
-  sensors <- read.delim(paste0("./",inst_idName$name[i],".txt"))
-  nrecs<-length(sensors[,1])
-  for (j in 1:nrecs){
-    rs<-dbGetQuery(con,paste0("insert into admin.sensors
-                            values(DEFAULT,'",sensors$Name[j],"','",sensors$Units[j],"',",inst_idName$id[i],");"))
+  if (file.size(paste0("./",inst_idName$name[i],".txt"))>1){
+    sensors <- read.delim(paste0("./",inst_idName$name[i],".txt"))
+    nrecs<-length(sensors[,1])
+    for (j in 1:nrecs){
+      rs<-dbGetQuery(con,paste0("insert into admin.sensor
+                              values(DEFAULT,'",sensors$Name[j],"','",sensors$Units[j],"',",inst_idName$id[i],");"))
+    }
   }
+}
+
+##### Add Participant ####
+# Read info from file (file is hidden)
+participant <- read.delim('./participants.txt')
+nrecs<-length(participant[,1])
+# Run inserts to the DB
+for (i in 1:nrecs){
+  rs<-dbGetQuery(con,paste0("insert into confidential.participant(id,name,houseid,email,phone,address)
+    values(DEFAULT,'",
+        as.character(participant$Name[i]),"',1,'",
+        as.character(participant$Email[i]),"','",
+        as.character(participant$Phone[i]),"','",
+        as.character(participant$Address[i]),"');"))
 }
 dbDisconnect(con)
