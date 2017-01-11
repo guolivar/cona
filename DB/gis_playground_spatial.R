fcac4fedfff73c50e4c9d3df5521d3bb70073035
olivaresga <olivaresga@niwa-30431.niwa.local>
2017-01-11 14:51:20 +1300

    I discovered data.table!!!

--- DB/gis_playground_spatial.R
+++ DB/gis_playground_spatial.R
@@ -11,12 +11,12 @@ library(rgdal)
 ##### Set the working directory DB ####
 setwd("~/repositories/cona/DB")
 ##### Read the credentials file (hidden) ####
-access <- read.delim("./.cona_login")
+access <- read.delim("./.cona_login", stringsAsFactors = FALSE)
 ##### Open the connection to the DB ####
 p <- dbDriver("PostgreSQL")
 con<-dbConnect(p,
-               user='cona_data',
-               password='c0nad@ta',
+               user=access$usr[2],
+               password=access$pwd[2],
                host='penap-data.dyndns.org',
                dbname='cona',
                port=5432)
--- DB/gis_playground_tseries.R
+++ DB/gis_playground_tseries.R
@@ -6,25 +6,25 @@ library(reshape2)
 ##### Set the working directory DB ####
 setwd("~/repositories/cona/DB")
 ##### Read the credentials file (hidden) ####
-access <- read.delim("./.cona_login")
+access <- read.delim("./.cona_login", stringsAsFactors = FALSE)
 ##### Open the connection to the DB ####
 p <- dbDriver("PostgreSQL")
 con<-dbConnect(p,
-               user='cona_data',
-               password='c0nad@ta',
+               user=access$usr[2],
+               password=access$pwd[2],
                host='penap-data.dyndns.org',
                dbname='cona',
                port=5432)
 
 
 ##### Get data ####
-data <- dbGetQuery(con,"SELECT d.recordtime at time zone 'NZST' as date, d.value as pm25, fs.geom as geom
+data <- dbGetQuery(con,"SELECT d.recordtime at time zone 'NZST' as date, d.value as pm25, i.serialn as instrument, fs.geom as geom
                    FROM data.fixed_data as d, admin.sensor as s, admin.instrument as i, admin.fixedsites as fs
                    WHERE s.id = d.sensorid AND
                    s.instrumentid = i.id AND
                    fs.id = d.siteid AND
+                   fs.id = 18 AND
                    i.name = 'ODIN-SD-3' AND
-                   i.serialn = 'ODIN-102' AND
                    s.name = 'PM2.5'")
 data$pm2.5 <- as.numeric(gsub("'","",data$pm25))
 wide_data <- dcast(data,date~instrument,value.var = 'pm2.5',fun.aggregate = mean)
