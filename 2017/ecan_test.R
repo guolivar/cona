3e54ee8f5c7afd94c6b3b5e6c78bc6e5f41490c6
Gustavo Olivares <guolivar@gmail.com>
2017-07-28 01:25:46 +1200

    ECan co-location done

--- 2017/ecan_test.R
+++ 2017/ecan_test.R
@@ -4,7 +4,7 @@ library(openair)
 library(ggplot2)
 # Paths and constants
 data_folder <- "~/data/CONA/2017/ODIN/ecan_test/"
-ecan_data_file <- "data_catalogue.csv"
+ecan_data_file <- "RangioraJune2017.txt"
 data_file <- dir(data_folder,'odin*')
 nfiles <- length(data_file)
 serials <- gsub('.TXT','',gsub('_','-',toupper(data_file))) # get serialn from file names
@@ -12,18 +12,18 @@ serials <- gsub('.TXT','',gsub('_','-',toupper(data_file))) # get serialn from f
 # Load data
 # Date corrections
 time_corrections <- read_delim(paste0(data_folder,"time_corrections.txt"),
-                               "\t")
-time_corrections$X3 <- NULL
-time_corrections$real_time <- as.POSIXct(as.character(time_corrections$real_time),tz='NZST')
+                               "\t", escape_double = FALSE,
+                               col_types = cols(real_time = col_character()),
+                               trim_ws = TRUE)
+time_corrections$real_time <- as.POSIXct(time_corrections$real_time, tz='UTC') - 12*3600
 
 # Go through files
 for (i in (1:nfiles)){
   t_idx <- which(time_corrections$serialn==serials[i])
-  print(time_corrections[t_idx,])
   odin <- read_delim(paste0(data_folder,data_file[i]),
                      "\t", escape_double = FALSE, col_types = cols(Day = col_character(),
                                                                    Time = col_character()), trim_ws = TRUE)
-  odin$date <- as.POSIXct(paste(odin$Day,odin$Time), tz='NZST')
+  odin$date <- as.POSIXct(paste(odin$Day,odin$Time), tz='UTC') - 12*3600
   dt <- time_corrections$real_time[t_idx] - odin$date[1]
   odin$date <- odin$date + dt
   odin$serialn <- serials[i]
@@ -46,10 +46,11 @@ ggplot(data=all_odin) +
   ggtitle("PM10","ECan test")
 
 # Load ECan's data
-ecan_data <- read_csv(paste0(data_folder,ecan_data_file))
-ecan_data$date <- as.POSIXct(ecan_data$DateTime,format = "%d/%m/%Y %I:%M:%S %p")
+ecan_data <- read_delim(paste0(data_folder,ecan_data_file),
+                        "\t", escape_double = FALSE, trim_ws = TRUE)
+ecan_data$date <- as.POSIXct(ecan_data$DateTime,format = "%d/%m/%Y %H:%M", tz='UTC') - 12*3600
 ecan_data$DateTime <- NULL
-names(ecan_data) <- c('Station','ws_max','T2m','ws','CO','T','PM10','T6m','PMc','wd','PM2.5','date')
+names(ecan_data) <- c('ws_max','ws','wd','CO','PM10','PM2.5','PMc','T2m','T6m','T','date')
 ecan_data.1hr <- timeAverage(ecan_data,avg.time = '1 hour')
 rm(odin)
 # Compare ODIN data against ECan
@@ -61,26 +62,33 @@ for (i in (1:nfiles)){
   odin.1hr.pm <- odin.1hr[,c('date',pm)]
   names(odin.1hr.pm) <- c('date',paste0(pm,serials[i]))
   ecan_data.1hr.pm <- ecan_data.1hr[,c('date',pm)]
-  merged_data <- merge(ecan_data.1hr.pm,odin.1hr.pm, by='date',all=TRUE)
+  merged_data <- merge(ecan_data.1hr.pm,odin.1hr.pm, by='date')
+  if (length(merged_data$date) < 5){
+    print("Too few datapoints")
+    next
+  }
   timePlot(selectByDate(merged_data,start = '2017-06-06',end = '2017-06-21'),
            pollutant = names(merged_data)[2:3],
+           group = TRUE,
            main = serials[i])
   # PM2.5
   pm <- 'PM2.5'
   odin.1hr.pm <- odin.1hr[,c('date',pm)]
   names(odin.1hr.pm) <- c('date',paste0(pm,serials[i]))
   ecan_data.1hr.pm <- ecan_data.1hr[,c('date',pm)]
-  merged_data <- merge(ecan_data.1hr.pm,odin.1hr.pm, by='date',all=TRUE)
+  merged_data <- merge(ecan_data.1hr.pm,odin.1hr.pm, by='date')
   timePlot(selectByDate(merged_data,start = '2017-06-06',end = '2017-06-21'),
            pollutant = names(merged_data)[2:3],
+           group = TRUE,
            main = serials[i])
   
   # Temperature
   odin.1hr.pm <- odin.1hr[,c('date','Temperature')]
   names(odin.1hr.pm) <- c('date',paste0('Temperature-',serials[i]))
   ecan_data.1hr.pm <- ecan_data.1hr[,c('date','T2m','T6m','T')]
-  merged_data <- merge(ecan_data.1hr.pm,odin.1hr.pm, by='date',all=TRUE)
+  merged_data <- merge(ecan_data.1hr.pm,odin.1hr.pm, by='date')
   timePlot(selectByDate(merged_data,start = '2017-06-01',end = '2017-06-21'),
            pollutant = names(merged_data)[2:5],
+           group = TRUE,
            main = serials[i])
 }
